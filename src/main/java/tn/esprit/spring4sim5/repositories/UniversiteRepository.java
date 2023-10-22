ec873aa98e86c11a62698ffb620055c00d868328
aymenzouaoui <aymen.zouaoui@esprit.tn>
2023-10-22 19:22:43 +0100

    Ajout de l annotation au Repository

--- src/main/java/tn/esprit/spring4sim5/repositories/UniversiteRepository.java
+++ src/main/java/tn/esprit/spring4sim5/repositories/UniversiteRepository.java
@@ -1,9 +1,10 @@
 package tn.esprit.spring4sim5.repositories;
 
+import org.springframework.stereotype.Repository;
 import tn.esprit.spring4sim5.entiies.Universite;
 
 import java.util.Optional;
-
+@Repository
 public class UniversiteRepository implements IUniversiteRepository{
     @Override
     public <S extends Universite> S save(S entity) {
