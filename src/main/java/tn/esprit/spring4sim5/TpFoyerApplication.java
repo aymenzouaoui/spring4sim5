de6ea90a01261ad9c79e4aab48a1cdb76c72d659
aymenzouaoui <aymen.zouaoui@esprit.tn>
2023-11-20 02:27:41 +0100

    enable crown

--- src/main/java/tn/esprit/spring4sim5/TpFoyerApplication.java
+++ src/main/java/tn/esprit/spring4sim5/TpFoyerApplication.java
@@ -2,8 +2,10 @@ package tn.esprit.spring4sim5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TpFoyerApplication {
 
 	public static void main(String[] args) {
